d6b96cf427220ef50d3eadd64d1be554b06aeb62
Nicoli kauani <153875941+nicolinicknk@users.noreply.github.com>
2025-11-02 17:09:16 +0000

    Para facilitar a leitura do terminal

---
+++ Resultado no terminal.c
@@ -0,0 +1,57 @@
+/* Para facilitar a leitura dos resultados no terminal, este programa imprime uma mensagem simples.
+
+== Cadastro Inicial dos Territórios ==
+
+Território 1:
+  Nome: Índia
+  Cor do exército: Laranja
+  Número de tropas: 15
+
+Território 2:
+  Nome: Estados Unidos
+  Cor do exército: Vermelho
+  Número de tropas: 21
+
+Território 3:
+  Nome: Espanha
+  Cor do exército: Amarelo
+  Número de tropas: 16
+
+Território 4:
+  Nome: Brasil
+  Cor do exército: Verde
+  Número de tropas: 22
+
+Território 5:
+  Nome: Argentina
+  Cor do exército: Azul
+  Número de tropas: 20
+
+== Estado atual do mapa ==
+-------------------------------
+Território 1
+  Nome   : Índia
+  Cor    : Laranja
+  Tropas : 15
+-------------------------------
+Território 2
+  Nome   : Estados Unidos
+  Cor    : Vermelho
+  Tropas : 21
+-------------------------------
+Território 3
+  Nome   : Espanha
+  Cor    : Amarelo
+  Tropas : 16
+-------------------------------
+Território 4
+  Nome   : Brasil
+  Cor    : Verde
+  Tropas : 22
+-------------------------------
+Território 5
+  Nome   : Argentina
+  Cor    : Azul
+  Tropas : 20
+-------------------------------
+*/
